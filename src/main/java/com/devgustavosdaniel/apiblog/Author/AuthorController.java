ddfda4f954bd112b556e20dbaa4bda4278ef4dd8
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-05-12 20:18:52 -0300

    feat: criando o metodo post no controller

--- src/main/java/com/devgustavosdaniel/apiblog/Author/AuthorController.java
+++ src/main/java/com/devgustavosdaniel/apiblog/Author/AuthorController.java
@@ -1,8 +1,15 @@
 package com.devgustavosdaniel.apiblog.Author;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/authors")
@@ -12,7 +19,17 @@ public class AuthorController {
     public final AuthorService authorService;
     public final AuthorMapper authorMapper;
 
-    
+    @PostMapping()
+    public ResponseEntity<AuthorResponseDTO> createAuthor(@Validated @RequestBody AuthorRequestDTO authorRequestDTO){
+        Author createAuthor = authorService.create(authorRequestDTO);
+        AuthorResponseDTO authorResponseDTO = authorMapper.toAuthorResponseDTO(createAuthor);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("{id}")
+                .buildAndExpand(authorResponseDTO.id())
+                .toUri();
+        return ResponseEntity.created(uri).body(authorResponseDTO);
+
+    }
 
 
 }
